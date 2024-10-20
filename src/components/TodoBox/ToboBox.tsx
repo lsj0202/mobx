40303cf8f1ca3d7a51c48684c8fdd4426c058a24
lsj0202 <w1004123@gmail.com>
2024-10-20 19:13:10 +0900

    U 추가

--- src/components/TodoBox/ToboBox.tsx
+++ src/components/TodoBox/ToboBox.tsx
@@ -1,6 +1,9 @@
-import st from './TodoBox.module.scss'
+import st from './TodoBox.module.scss';
 import { AiOutlineDelete } from "react-icons/ai";
+import { FaRegPenToSquare } from "react-icons/fa6";
 import TodoBoxVm from './TodoBoxVm';
+import { useState } from 'react';
+import { observer } from 'mobx-react';
 
 type TodoBoxProps = {
   id: number;
@@ -8,16 +11,55 @@ type TodoBoxProps = {
   author: string;
 }
 
-function TodoBox({ id, contents, author }: TodoBoxProps) {
+const TodoBox = observer(({ id, contents, author }: TodoBoxProps) => {
+  const [modifyTodo, setModifyTodo] = useState(false);
+  const [newContents, setNewContents] = useState(contents);
+  const [newAuthor, setNewAuthor] = useState(author);
+
+  const handleUpdate = () => {
+    TodoBoxVm.changeTodo(id, newContents, newAuthor);
+    setModifyTodo(false);
+  };
+
   return (
     <div className={st.container}>
-      <p className={st.contents}>{contents}</p>
-      <div className={st.authorWrapper}>
-        <AiOutlineDelete className={st.delete} onClick={() => TodoBoxVm.deleteTodo(id)}/>
-        <span>작성자: {author}</span>
-      </div>
+      {modifyTodo ? (
+        <>
+          <textarea
+            className={st.contentsTextarea}
+            value={newContents}
+            onChange={(e) => setNewContents(e.target.value)}
+          />
+          <div className={st.authorWrapper}>
+            <div className={st.iconWrapper}>
+              <AiOutlineDelete className={st.delete} onClick={() => TodoBoxVm.deleteTodo(id)} />
+              <FaRegPenToSquare className={st.update} onClick={() => setModifyTodo(!modifyTodo)} />
+            </div>
+            <div className={st.submitWrapper}>
+              <input
+                className={st.authorInput}
+                type='text'
+                value={newAuthor}
+                onChange={(e) => setNewAuthor(e.target.value)}
+              />
+              <button className={st.button} onClick={handleUpdate}>제출</button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className={st.contents}>{contents}</p>
+          <div className={st.authorWrapper}>
+            <div className={st.iconWrapper}>
+              <AiOutlineDelete className={st.delete} onClick={() => TodoBoxVm.deleteTodo(id)} />
+              <FaRegPenToSquare className={st.update} onClick={() => setModifyTodo(true)} />
+            </div>
+            <span>작성자: {author}</span>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+});
 
 export default TodoBox;
--- src/components/TodoBox/TodoBoxVm.ts
+++ src/components/TodoBox/TodoBoxVm.ts
@@ -4,6 +4,30 @@ class TodoBoxVm {
   deleteTodo(id: number) {
     todoModel.todoList = todoModel.todoList.filter(todo => todo.id !== id);
   }
+
+  get contents() {
+    return todoModel.contents;
+  }
+
+  get author() {
+    return todoModel.author;
+  }
+
+  setNewContents(value: string) {
+    todoModel.contents = value;
+  }
+
+  setNewAuthor(value: string) {
+    todoModel.author = value;
+  }
+
+  changeTodo(id: number, contents: string, author: string) {
+    const todo = todoModel.todoList.find(todo => todo.id === id);
+    if (todo) {
+      todo.contents = contents;
+      todo.author = author;
+    }
+  }
 }
 
 export default new TodoBoxVm();
